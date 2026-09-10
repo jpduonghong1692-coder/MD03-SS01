57479b7c391e17e79f310d2f431e3f4df23e088b
jpduonghong1692-coder <jpduonghong1692@gmail.com>
2026-09-10 16:17:49 +0900

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,13 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        String ten = "Hong";
+        int tuoi = 23;
+        String diaChi = "Việt Nam";
+
+        System.out.println(
+                "Tên tôi là Hồng , tôi 24 tuổi, hiện tại đang học tại Rikkei. Quê ở Phú Thọ ");
+    }
+}
